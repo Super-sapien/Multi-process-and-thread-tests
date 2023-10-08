d05d0da67199d97854b7173f4acd89ec1b0a3bb2
jdh88 <jdh88@uclive.ac.nz>
2023-10-08 17:54:13 +1300

    initial commit

---
+++ process.c
@@ -0,0 +1,108 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+/* the more threads you have, the faster it runs, the less accurate it is? */
+#define MAX_CHILDREN 5
+static int numChildren = 0;
+
+typedef double MathFunc_t(double);
+
+
+double gaussian(double x)
+{
+	return exp(-(x*x)/2) / (sqrt(2 * M_PI));
+}
+
+
+double chargeDecay(double x)
+{
+	if (x < 0) {
+		return 0;
+	} else if (x < 1) {
+		return 1 - exp(-5*x);
+	} else {
+		return exp(-(x-1));
+	}
+}
+
+#define NUM_FUNCS 3
+static MathFunc_t* const FUNCS[NUM_FUNCS] = {&sin, &gaussian, &chargeDecay};
+
+
+
+
+
+//Integrate using the trapezoid method. 
+double integrateTrap(MathFunc_t* func, double rangeStart, double rangeEnd, size_t numSteps)
+{
+	double rangeSize = rangeEnd - rangeStart;
+	double dx = rangeSize / numSteps;
+
+	double area = 0;
+	for (size_t i = 0; i < numSteps; i++) {
+		double smallx = rangeStart + i*dx;
+		double bigx = rangeStart + (i+1)*dx;
+
+		area += dx * ( func(smallx) + func(bigx) ) / 2; //Would be more efficient to multiply area by dx once at the end. 
+	}
+
+	return area;
+}
+
+
+
+
+bool getValidInput(double* start, double* end, size_t* numSteps, size_t* funcId)
+{
+	printf("Query: [start] [end] [numSteps] [funcId]\n");
+
+	//Read input numbers and place them in the given addresses:
+	size_t numRead = scanf("%lf %lf %zu %zu", start, end, numSteps, funcId);
+
+	//Return whether the given range is valid:
+	return (numRead == 4 && *end >= *start && *numSteps > 0 && *funcId < NUM_FUNCS);
+}
+
+void childDied() {
+	numChildren--;
+	signal(SIGCHLD, childDied);
+	// wait(NULL);
+}
+
+int main(void)
+{
+	double rangeStart;
+	double rangeEnd;
+	size_t numSteps;
+	size_t funcId;
+
+	pid_t childPid;
+	signal(SIGCHLD, childDied);  /* register signal for when child stopped or terminated */
+	while (getValidInput(&rangeStart, &rangeEnd, &numSteps, &funcId)) { // parent reprompts for input by calling getValidInput
+		/* wait here for num children to be less than max children */
+		/* TODO: change numChildren to a semaphore */
+		while (numChildren >= MAX_CHILDREN) {
+			wait(NULL);
+		}
+		if (numChildren < MAX_CHILDREN) {
+			
+			childPid = fork();
+			if (childPid == 0) { // inside child process
+				double area = integrateTrap(FUNCS[funcId], rangeStart, rangeEnd, numSteps); // child runs integrateTrap
+				printf("The integral of function %zu in range %g to %g is %.10g\n", funcId, rangeStart, rangeEnd, area);
+				_exit(0); // forces exit immediately
+			} else {
+				numChildren++;
+
+			}
+		};
+	}
+	exit(1);
+}
---
+++ thread.c
@@ -0,0 +1,126 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define MAX_THREADS 4
+
+
+typedef double MathFunc_t(double);
+
+typedef struct {
+    MathFunc_t *func;
+    double rangeStart;
+    double rangeEnd;
+	size_t numSteps;
+    double *total;
+
+    pthread_mutex_t *lock;
+    pthread_t thread;
+
+} Worker;
+
+
+double gaussian(double x)
+{
+	return exp(-(x*x)/2) / (sqrt(2 * M_PI));
+}
+
+
+double chargeDecay(double x)
+{
+	if (x < 0) {
+		return 0;
+	} else if (x < 1) {
+		return 1 - exp(-5*x);
+	} else {
+		return exp(-(x-1));
+	}
+}
+
+#define NUM_FUNCS 3
+static MathFunc_t* const FUNCS[NUM_FUNCS] = {&sin, &gaussian, &chargeDecay};
+
+
+
+
+
+//Integrate using the trapezoid method. 
+// pass in worker to integrateTrap
+double integrateTrap(void *ptr)
+{
+    Worker *worker = (Worker*)ptr;
+	double rangeSize = worker->rangeEnd - worker->rangeStart;
+	double dx = rangeSize / worker->numSteps;
+
+	double area = 0;
+	for (size_t i = 0; i < worker->numSteps; i++) {
+		double smallx = worker->rangeStart + i*dx;
+		double bigx = worker->rangeStart + (i+1)*dx;
+
+		area += dx * ( worker->func(smallx) + worker->func(bigx) ) / 2; //Would be more efficient to multiply area by dx once at the end. 
+	}
+
+    // do the mutex locking and unlocking while changing total
+    pthread_mutex_lock(worker->lock); // lock other processes out of critical region
+    (*worker->total)++; // increment the total
+    pthread_mutex_unlock(worker->lock); // unlock so other processes can access if needed
+    
+	return area;
+}
+
+
+
+
+bool getValidInput(double* start, double* end, size_t* numSteps, size_t* funcId)
+{
+	printf("Query: [start] [end] [numSteps] [funcId]\n");
+
+	//Read input numbers and place them in the given addresses:
+	size_t numRead = scanf("%lf %lf %zu %zu", start, end, numSteps, funcId);
+
+	//Return whether the given range is valid:
+	return (numRead == 4 && *end >= *start && *numSteps > 0 && *funcId < NUM_FUNCS);
+}
+
+
+
+int main(void)
+{
+	double rangeStart;
+	double rangeEnd;
+	size_t numSteps;
+	size_t funcId;
+    pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // initialize mutex
+    Worker workers[MAX_THREADS];
+    double total = 0;
+
+	while (getValidInput(&rangeStart, &rangeEnd, &numSteps, &funcId)) {
+        double totalRange = rangeEnd - rangeStart;
+        double stepSize = totalRange / MAX_THREADS;
+        for (int i = 0; i < MAX_THREADS; ++i) {
+            Worker *worker = &workers[i]; // Create workers
+            worker->total = &total; // Pass the global total into each thread
+            worker->numSteps = totalRange / stepSize;
+            worker->rangeStart = rangeStart;
+            worker->rangeEnd = rangeEnd;
+            worker->func = FUNCS[funcId];
+            worker->lock = &lock; // init the workers mutex
+            pthread_create(&worker->thread, NULL, double (*)(integrateTrap(worker)), NULL);
+        }
+        for (int i = 0; i < MAX_THREADS; ++i) {
+            pthread_join(workers[i].thread, NULL); // wait for each thread to terminate
+        }
+
+		// double area = integrateTrap(FUNCS[funcId], rangeStart, rangeEnd, numSteps);
+
+		printf("The integral of function %zu in range %g to %g is %.10g\n", funcId, rangeStart, rangeEnd, total);
+	}
+
+	exit(0);
+}
